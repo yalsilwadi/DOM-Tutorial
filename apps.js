21756bbef7c574eece2d760b945842db38dfa91d
yalsilwadi <yasseralsilwadi@gmail.com>
2023-05-17 18:24:27 -0500

    added more lessons

--- apps.js
+++ apps.js
@@ -110,3 +110,42 @@ link.addEventListener('click', function(e){
   e.preventDefault();
   console.log('Navigation to', e.target.textContent, 'was prevented');
 });
+
+// Event Bubbling:
+
+const list = document.querySelector('#book-list ul');
+
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
+
+// Interacting with Forms:
+
+const list1 = document.querySelector('#book-list ul');
+
+// delete books
+list1.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
+
+const forms = document.forms;
+console.log(forms);
+console.log(forms['add-book']);
+
+Array.from(forms).forEach(function(form){
+  console.log(form);
+});
+
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+});
